2e686f88e412599a0c339bf015cc296e0441f07a
vivek <wewakeverma1990@gmail.com>
2023-11-03 06:47:44 +0530

    updated controller models utils for mime type

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -5,17 +5,18 @@ import File from "../models/file.js";
 export const uploadImage = async (request, response) => {
     const fileObj = {
         path: request.file.path,
-        name: request.file.originalname
+        name: request.file.originalname,
+        mimeType: request.file.mimetype // Capture the MIME type
     }
     try {
         const file = await File.create(fileObj);
         const baseUrl = process.env.PORT || 'https://easyshare-2-0.onrender.com' || 'http://localhost:8000';
-        response.status(200).json({ path: `${baseUrl}/file/${file._id}` })
+        response.status(200).json({ path: `${baseUrl}/file/${file._id}` });
     } catch (error) {
        console.error(error.message);
-       response.status(500).json({ error: error.message })
+       response.status(500).json({ error: error.message });
     }
-}
+};
 
 export const downloadImage = async (request, response) => {
     try {
@@ -27,9 +28,14 @@ export const downloadImage = async (request, response) => {
 
         // Set Content-Disposition header to force download with the file name
         response.setHeader('Content-Disposition', `attachment; filename="${file.name}"`);
+
+        // Set Content-Type header with the stored MIME type
+        response.setHeader('Content-Type', file.mimeType);
+
         response.sendFile(file.path);
     } catch (error) {
         console.error(error.message);
         return response.status(500).json({ error: error.message });  
     }
 }
+
--- models/file.js
+++ models/file.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-
 const fileSchema = new mongoose.Schema({
     path: {
         type: String,
@@ -10,12 +9,16 @@ const fileSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    mimeType: {
+        type: String, // Store the MIME type as a string
+        required: true // Update or remove this line as per requirement
+    },
     downloadContent: {
         type: Number,
         required: true,
         default: 0
     }
-})
+});
 
 const File = mongoose.model('file', fileSchema);
 
